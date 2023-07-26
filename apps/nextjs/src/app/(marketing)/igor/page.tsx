5bbab15d6be7e1cc26c79a94e6b0c328a7c7ff06
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-26 14:38:18 +0100

    feat(app): add more Tabs to tab example in /igor

--- apps/nextjs/src/app/(marketing)/igor/page.tsx
+++ apps/nextjs/src/app/(marketing)/igor/page.tsx
@@ -76,9 +76,11 @@ export default function Home() {
           <TabsList>
             <TabsTrigger value="account">Account</TabsTrigger>
             <TabsTrigger value="password">Password</TabsTrigger>
+            <TabsTrigger value="other">other</TabsTrigger>
           </TabsList>
           <TabsContent value="account">Make changes to your account here.</TabsContent>
           <TabsContent value="password">Change your password here.</TabsContent>
+          <TabsContent value="other">Other here.</TabsContent>
         </Tabs>
 
         <ToastDemo />
