83fed05f2c2be47539fc368f97e37578610c8088
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-26 21:34:40 +0100

    feat(app): org-dropdown uses dropdownCheckItem instead of handling checks itself

--- apps/nextjs/src/app/(dashboard)/_components/organization-dropdown.tsx
+++ apps/nextjs/src/app/(dashboard)/_components/organization-dropdown.tsx
@@ -1,5 +1,4 @@
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator } from "@haxiom/ui/dropdown-menu";
-import { Check } from "@haxiom/ui/icons";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuCheckboxItem } from "@haxiom/ui/dropdown-menu";
 import { toast } from "@haxiom/ui/use-toast";
 import type { ReactNode } from "react";
 
@@ -27,16 +26,15 @@ export const OrgDropdown = ({children}:{children: ReactNode}) => {
              <DropdownMenuGroup>
                <DropdownMenuLabel>{MOCK_ORGS.length} Organizations</DropdownMenuLabel>
                {MOCK_ORGS.map((org) => (
-                  <DropdownMenuItem key={org.id} onClick={() => {
+                  <DropdownMenuCheckboxItem checked={MOCK_CURRENT_ORG?.id === org.id} key={org.id} onClick={() => {
                      toast({
                        title: "Pretend you switched orgs",
                        description:
                          "If you're seeing this, I didn't get through all my TODOs.",
                      });
                    }}>
-                  <span>{org.name}</span>
-                  {MOCK_CURRENT_ORG?.id === org.id && <Check className="ml-auto h-4 w-4" />}
-                </DropdownMenuItem>
+                  {org.name}
+                </DropdownMenuCheckboxItem>
                ))}
              </DropdownMenuGroup>
              <DropdownMenuSeparator />
