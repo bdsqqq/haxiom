cbdaf17b6056ed45519237d9db7f08a9150accf0
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-26 13:58:18 +0100

    chore(app): use new color tokens

--- apps/nextjs/src/app/(marketing)/igor/_components/command-palette-demo.tsx
+++ apps/nextjs/src/app/(marketing)/igor/_components/command-palette-demo.tsx
@@ -39,7 +39,7 @@ export function CommandDialogDemo() {
     <>
       <p className="text-sm text-subtle">
         Press{" "}
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-subtle opacity-100">
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-subtle px-1.5 font-mono text-[10px] font-medium text-subtle opacity-100">
           <span className="text-xs">⌘</span>J
         </kbd>
       </p>
