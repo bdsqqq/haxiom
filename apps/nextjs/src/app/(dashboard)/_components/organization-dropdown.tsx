77aaebf51017521e5250aa5613c1d3bfef77d773
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-26 22:00:46 +0100

    feat(app): use new-organization-dialog in organization-dropdown

--- apps/nextjs/src/app/(dashboard)/_components/organization-dropdown.tsx
+++ apps/nextjs/src/app/(dashboard)/_components/organization-dropdown.tsx
@@ -1,6 +1,8 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuCheckboxItem } from "@haxiom/ui/dropdown-menu";
 import { toast } from "@haxiom/ui/use-toast";
 import type { ReactNode } from "react";
+import { NewOrganizationDialog } from "./new-organization-dialog";
+import { DialogTrigger } from "@haxiom/ui/dialog";
 
 const MOCK_ORGS = [
    {
@@ -20,6 +22,7 @@ export const MOCK_CURRENT_ORG = MOCK_ORGS[0];
  */
 export const OrgDropdown = ({children}:{children: ReactNode}) => {
       return (
+        <NewOrganizationDialog>
          <DropdownMenu>
             {children}
            <DropdownMenuContent align="end" side="bottom">
@@ -38,16 +41,13 @@ export const OrgDropdown = ({children}:{children: ReactNode}) => {
                ))}
              </DropdownMenuGroup>
              <DropdownMenuSeparator />
-             <DropdownMenuItem onClick={() => {
-                     toast({
-                       title: "Pretend you're in the org creation flow now",
-                       description:
-                         "If you're seeing this, I didn't get through all my TODOs.",
-                     });
-                   }}>
-               Create Organization
-             </DropdownMenuItem>
+             <DropdownMenuItem asChild>
+                  <DialogTrigger>
+                    Create Organization
+                  </DialogTrigger>    
+                </DropdownMenuItem>
              </DropdownMenuContent>
          </DropdownMenu>
+         </NewOrganizationDialog>
        );
 }
