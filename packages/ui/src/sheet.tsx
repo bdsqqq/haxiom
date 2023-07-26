45701356bd5e22be5b8bcc73a92c70f3a65d8479
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-26 12:34:55 +0100

    feat(ui): make sheet overlay blur optional

--- packages/ui/src/sheet.tsx
+++ packages/ui/src/sheet.tsx
@@ -40,13 +40,18 @@ const SheetPortal = ({
 );
 SheetPortal.displayName = SheetPrimitive.Portal.displayName;
 
+type SheetOverlayProps = React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay> & {
+  blur?: boolean;
+};
+
 const SheetOverlay = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
->(({ className, ...props }, ref) => (
+  SheetOverlayProps
+>(({ className, blur = true, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      "bg/80 fixed inset-0 z-50 backdrop-blur-sm transition-all duration-fast-01 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in",
+      "bg/80 fixed inset-0 z-50 transition-all duration-fast-01 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in",
+      blur && "backdrop-blur-sm",
       className,
     )}
     {...props}
@@ -144,18 +149,22 @@ const sheetVariants = cva(
 );
 
 export interface DialogContentProps
-  extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-    VariantProps<typeof sheetVariants> {}
+  extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>
+    {
+      options?: VariantProps<typeof sheetVariants> & {
+        blur?: boolean;
+      };
+    }
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   DialogContentProps
->(({ position, size, className, children, ...props }, ref) => (
-  <SheetPortal position={position}>
-    <SheetOverlay />
+>(({ options, className, children, ...props }, ref) => (
+  <SheetPortal position={options?.position}>
+    <SheetOverlay blur={options?.blur} />
     <SheetPrimitive.Content
       ref={ref}
-      className={cn(sheetVariants({ position, size }), className)}
+      className={cn(sheetVariants(options), className)}
       {...props}
     >
       {children}
