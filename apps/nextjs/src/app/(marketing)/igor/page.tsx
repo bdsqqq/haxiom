deaab968044ef0ceb3946a4cc20acd7c9f00c92d
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-26 09:48:14 +0100

    feat: add Card example to /igor page

--- apps/nextjs/src/app/(marketing)/igor/page.tsx
+++ apps/nextjs/src/app/(marketing)/igor/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@haxiom/ui/button";
 import { Input, TextArea } from "@haxiom/ui/input";
 import { Dialog, DialogContent, DialogTrigger } from "@haxiom/ui/dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "@haxiom/ui/avatar";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@haxiom/ui/card";
 
 export const runtime = "edge";
 
@@ -16,6 +17,15 @@ export default function Home() {
           <AvatarImage src="https://github.com/bdsqqq.png" />
           <AvatarFallback>IB</AvatarFallback>
         </Avatar>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>Card Content</CardContent>
+          <CardFooter>Card Footer</CardFooter>
+        </Card>
 
         <Dialog>
           <DialogTrigger asChild>
