b7fd359d9ce9d9446da0de41b1cca48ec791d41d
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-26 09:54:15 +0100

    feat: add Checkbox example to /igor page

--- apps/nextjs/src/app/(marketing)/igor/page.tsx
+++ apps/nextjs/src/app/(marketing)/igor/page.tsx
@@ -3,6 +3,7 @@ import { Input, TextArea } from "@haxiom/ui/input";
 import { Dialog, DialogContent, DialogTrigger } from "@haxiom/ui/dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "@haxiom/ui/avatar";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@haxiom/ui/card";
+import { Checkbox } from "@haxiom/ui/checkbox";
 
 export const runtime = "edge";
 
@@ -26,6 +27,11 @@ export default function Home() {
           <CardContent>Card Content</CardContent>
           <CardFooter>Card Footer</CardFooter>
         </Card>
+
+        <fieldset className="inline-flex gap-1 items-center">
+          <Checkbox name="checkbox" />
+          <label htmlFor="checkbox">Checkbox</label>
+        </fieldset>
 
         <Dialog>
           <DialogTrigger asChild>
