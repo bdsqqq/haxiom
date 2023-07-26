19bee5a11802339bc59973201916b8a09ecf6df9
flbn <hi@woven.ink>
2023-07-26 00:14:06 -0400

    utility functions

---
+++ packages/ui/src/utils/fn.ts
@@ -0,0 +1,2 @@
+export const pick = <T extends object, K extends keyof T>(obj: T, ...keys: K[]) => !obj ? null : Object.fromEntries(keys.filter(key => key in obj).map(key => [key, obj[key]])) as Pick<T, K>;
+export const omit = <T extends object, K extends keyof T>(obj: T, ...keys: K[]) => !obj ? null : Object.fromEntries(Object.entries(obj).filter(([key]) => !keys.includes(key as K))) as Omit<T, K>
