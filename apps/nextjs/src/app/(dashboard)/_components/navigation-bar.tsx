8bf5230f143cc89246272ca36d728984660e028b
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-27 03:10:30 +0100

    fix(app): navigates correctly to top level paths

--- apps/nextjs/src/app/(dashboard)/_components/navigation-bar.tsx
+++ apps/nextjs/src/app/(dashboard)/_components/navigation-bar.tsx
@@ -1,34 +1,34 @@
-"use client";
+'use client';
 
-import { Axiom, ChevronDown, Settings } from "@haxiom/ui/icons";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { UserDropdown } from "./user-dropdown";
-import { cn } from "@haxiom/ui";
-import { MOCK_CURRENT_ORG, OrgDropdown } from "./organization-dropdown";
-import { DropdownMenuTrigger } from "@haxiom/ui/dropdown-menu";
-import { MAX_WIDTH_CLASS } from "../_constants";
+import { Axiom, ChevronDown, Settings } from '@haxiom/ui/icons';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { UserDropdown } from './user-dropdown';
+import { cn } from '@haxiom/ui';
+import { MOCK_CURRENT_ORG, OrgDropdown } from './organization-dropdown';
+import { DropdownMenuTrigger } from '@haxiom/ui/dropdown-menu';
+import { MAX_WIDTH_CLASS } from '../_constants';
 
 const NAV_ITEMS = [
   {
-    label: "Datasets",
-    href: "datasets",
+    label: 'Datasets',
+    href: '/datasets',
   },
   {
-    label: "Stream",
-    href: "stream",
+    label: 'Stream',
+    href: '/stream',
   },
   {
-    label: "Explore",
-    href: "explore",
+    label: 'Explore',
+    href: '/explore',
   },
   {
-    label: "Dashboards",
-    href: "dashboards",
+    label: 'Dashboards',
+    href: '/dashboards',
   },
   {
-    label: "Monitors",
-    href: "monitors",
+    label: 'Monitors',
+    href: '/monitors',
   },
 ];
 
@@ -40,14 +40,14 @@ export const NavigationBar = () => {
    * in Axiom proper, this would be false as the first path
    * segment is orgId.
    */
-  const topLevelPath = pathname.split("/")[1];
+  const topLevelPath = pathname.split('/')[1];
 
   return (
     <nav className="sticky top-0 bg border-b">
-      <div className={cn("flex justify-between h-12", MAX_WIDTH_CLASS)}>
+      <div className={cn('flex justify-between h-12', MAX_WIDTH_CLASS)}>
         <div className="flex items-center h-full">
           <Link
-            href={"/"}
+            href={'/'}
             className="hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 h-full px-2 -ml-2 block flex items-center"
           >
             <Axiom className="h-6 w-6" />
@@ -57,10 +57,8 @@ export const NavigationBar = () => {
             <Link
               key={href}
               className={cn(
-                "hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 h-full px-2 block flex items-center border-b -mb-px",
-                topLevelPath === href
-                  ? "border-b-gray-11"
-                  : "border-transparent",
+                'hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 h-full px-2 block flex items-center border-b -mb-px',
+                topLevelPath === href.split('/')[1] ? 'border-b-gray-11' : 'border-transparent'
               )}
               href={href}
             >
@@ -71,7 +69,7 @@ export const NavigationBar = () => {
           <Separator />
 
           <Link
-            href={"/settings"}
+            href={'/settings'}
             className="hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 h-full px-2 block flex items-center"
           >
             <Settings className="h-4 w-4" />
@@ -81,9 +79,9 @@ export const NavigationBar = () => {
           <Separator />
           <OrgDropdown>
             <DropdownMenuTrigger className="flex gap-1 hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 data-[state=open]:bg-element-hover h-full px-2 items-center">
-               {MOCK_CURRENT_ORG?.name} <ChevronDown className="h-4 w-4 mt-1" />
+              {MOCK_CURRENT_ORG?.name} <ChevronDown className="h-4 w-4 mt-1" />
             </DropdownMenuTrigger>
-            </OrgDropdown>
+          </OrgDropdown>
           <UserDropdown className="hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 data-[state=open]:bg-element-hover h-full px-2 -mr-2" />
         </div>
       </div>
