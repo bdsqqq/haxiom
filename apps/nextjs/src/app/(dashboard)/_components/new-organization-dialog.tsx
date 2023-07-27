d6f7ab6466fa09a925e8254954dbf630cf41c034
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-27 01:14:36 +0100

    feat(app): prettier + ringoffset 0 in new-org-dialog

--- apps/nextjs/src/app/(dashboard)/_components/new-organization-dialog.tsx
+++ apps/nextjs/src/app/(dashboard)/_components/new-organization-dialog.tsx
@@ -1,21 +1,14 @@
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogTitle,
-} from "@haxiom/ui/dialog";
-import type { ReactNode } from "react";
-import { Label } from "@haxiom/ui/label";
-import { Input } from "@haxiom/ui/input";
-import { Button } from "@haxiom/ui/button";
-import { toast } from "@haxiom/ui/use-toast";
+import { Dialog, DialogClose, DialogContent, DialogTitle } from '@haxiom/ui/dialog';
+import type { ReactNode } from 'react';
+import { Label } from '@haxiom/ui/label';
+import { Input } from '@haxiom/ui/input';
+import { Button } from '@haxiom/ui/button';
+import { toast } from '@haxiom/ui/use-toast';
 
 /**
  * Provide a DialogTrigger as a child to this component.
  */
-export const NewOrganizationDialog = (
-  { children }: { children?: ReactNode },
-) => {
+export const NewOrganizationDialog = ({ children }: { children?: ReactNode }) => {
   return (
     <Dialog>
       {children}
@@ -25,27 +18,24 @@ export const NewOrganizationDialog = (
           onSubmit={(e) => {
             e.preventDefault();
             toast({
-              title: "Pretend you created an org",
-              description:
-                "If you're seeing this, I didn't get through all my TODOs.",
+              title: 'Pretend you created an org',
+              description: "If you're seeing this, I didn't get through all my TODOs.",
             });
           }}
           className="flex flex-col gap-4"
         >
           <fieldset>
             <Label htmlFor="name">Name</Label>
-            <Input id="name" placeholder="Triangle company..." />
+            <Input className="ring-offset-0" id="name" placeholder="Triangle company..." />
           </fieldset>
 
           <div className="flex justify-between flex-row-reverse w-full">
-            <Button type="submit">
-              Create
-            </Button>
+            <Button type="submit">Create</Button>
 
             <DialogClose asChild>
               <Button
                 options={{
-                  variant: "outline",
+                  variant: 'outline',
                 }}
                 type="button"
               >
