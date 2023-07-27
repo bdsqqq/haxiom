d49198cbe7d5a510163318148d9f39adae8889d4
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-27 03:39:04 +0100

    refactor(app): controlled select-dataset to handle going back to empty state throgh nav

--- apps/nextjs/src/app/(dashboard)/datasets/_components/select-dataset.tsx
+++ apps/nextjs/src/app/(dashboard)/datasets/_components/select-dataset.tsx
@@ -3,25 +3,39 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@haxiom/ui/select';
 import { MOCK_DATASETS } from '~/app/(dashboard)/_constants';
 import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export const SelectDataset = () => {
   const router = useRouter();
   const pathname = usePathname();
 
   const urlSegments = pathname.split('/');
-  const datasetIdFromUrl = urlSegments[urlSegments.indexOf('datasets') + 1];
+  const datasetIdFromUrl = urlSegments[urlSegments.indexOf('datasets') + 1] ?? '';
+
+  const [selectedDatasetId, setSelectedDatasetId] = useState<string>(datasetIdFromUrl);
+
+  useEffect(() => {
+    console.log('datasetIdFromUrl changed', datasetIdFromUrl);
+    setSelectedDatasetId(datasetIdFromUrl);
+    router.push(`/datasets/${datasetIdFromUrl}`);
+  }, [datasetIdFromUrl, router]);
 
   return (
     <Select
-      defaultValue={datasetIdFromUrl ? datasetIdFromUrl : undefined}
-      onValueChange={(datasetId) => {
-        router.push(`/datasets/${datasetId}`);
+      defaultValue={datasetIdFromUrl ? datasetIdFromUrl : ''}
+      value={selectedDatasetId}
+      onValueChange={(newDatasetId) => {
+        setSelectedDatasetId(newDatasetId);
+        router.push(`/datasets/${newDatasetId}`);
       }}
     >
       <SelectTrigger className="w-[240px] ring-offset-0">
         <SelectValue placeholder="Select dataset..." />
       </SelectTrigger>
       <SelectContent sideOffset={0} alignOffset={0}>
+        <SelectItem value="" className="hidden">
+          Select dataset...
+        </SelectItem>
         {MOCK_DATASETS.map((dataset) => (
           <SelectItem key={dataset.id} value={dataset.id}>
             {dataset.name}
