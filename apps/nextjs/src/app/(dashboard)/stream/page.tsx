ac201357c1bfb4c255b97b00419d92c0e417b047
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-27 17:32:04 +0100

    feat(app): update stream page

--- apps/nextjs/src/app/(dashboard)/stream/page.tsx
+++ apps/nextjs/src/app/(dashboard)/stream/page.tsx
@@ -1,3 +1,19 @@
+import { cn } from '@haxiom/ui';
+import { MAX_WIDTH_CLASS, MAIN_CONTENT_CLASS } from '~/app/(dashboard)/_constants';
+import { DatasetsTable } from '~/app/(dashboard)/_components/datasets-table';
+import { StarredQueriesCard } from '~/app/(dashboard)/_components/starred-queries-card';
+
 export default function Page() {
-  return <div>stream page</div>;
+  return (
+    <main className={cn(MAX_WIDTH_CLASS, MAIN_CONTENT_CLASS)}>
+      <div className="grid gap-4 grid-cols-12">
+        <div className="col-span-8 row-start-1 row-end-[4]">
+          <DatasetsTable />
+        </div>
+        <div className="col-span-4 col-end-13 row-span-1">
+          <StarredQueriesCard />
+        </div>
+      </div>
+    </main>
+  );
 }
