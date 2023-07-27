8e3718181b96ea1b4f833e8be0ec486863efc4ee
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-27 03:02:17 +0100

    feat(app): expose `className` from top-tool-bar

--- apps/nextjs/src/app/(dashboard)/_components/top-tool-bar.tsx
+++ apps/nextjs/src/app/(dashboard)/_components/top-tool-bar.tsx
@@ -1,13 +1,11 @@
-import { cn } from "@haxiom/ui";
-import type { ReactNode } from "react";
-import { MAX_WIDTH_CLASS } from "../_constants";
+import { cn } from '@haxiom/ui';
+import type { ReactNode } from 'react';
+import { MAX_WIDTH_CLASS } from '../_constants';
 
-export const TopToolBar = ({ children }: { children?: ReactNode }) => {
+export const TopToolBar = ({ children, className }: { children?: ReactNode; className?: string }) => {
   return (
     <div className="h-16 border-b">
-      <div className={cn("flex h-full items-center", MAX_WIDTH_CLASS)}>
-        {children}
-      </div>
+      <div className={cn('flex h-full items-center', MAX_WIDTH_CLASS, className)}>{children}</div>
     </div>
   );
 };
