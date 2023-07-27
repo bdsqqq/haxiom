9d63a95e255e3adeb4add427e8ded7f79334c67a
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-27 18:50:26 +0100

    feat(app): update dashboards page + components needed for it

---
+++ apps/nextjs/src/app/(dashboard)/dashboards/_components/private-dashboards-card.tsx
@@ -0,0 +1,55 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@haxiom/ui/card';
+import { Logo, User } from '@haxiom/ui/icons';
+import Link from 'next/link';
+
+const MAX_DASHBOARDS = 5;
+// TODO: make MOCK_DASHBOARDS
+const DASHBOARDS: string[] = [];
+const DASHBOARDS_TO_RENDER = DASHBOARDS.slice(0, MAX_DASHBOARDS);
+
+export const PrivateDashboardsCard = () => {
+  return (
+    <Card className="rounded-none">
+      <CardHeader className="flex flex-col gap-2 items-start">
+        <CardTitle className="flex gap-2">
+          <User className="h-4 w-4" /> Private Dashboards
+        </CardTitle>
+        <CardDescription>Only you can see and change these dashboards</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center p-0">
+        {DASHBOARDS_TO_RENDER.length ? (
+          DASHBOARDS_TO_RENDER.map((Dashboard) => <PrivateDashboard key={Dashboard} />)
+        ) : (
+          <EmptyState />
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+const EmptyState = () => {
+  return (
+    <div className="flex flex-col items-center p-4">
+      <p>no queries in history</p>
+    </div>
+  );
+};
+
+// TODO: Update with actual content once MOCK_DASHBOARDS is done
+const PrivateDashboard = () => {
+  return (
+    <Link
+      className="w-full flex justify-between h-full p-4 hover:bg-element-hover focus:bg-element-hover focus:ring-offset-0 [&:not(:last-child)]:border-b"
+      href={''}
+    >
+      <div className="flex gap-2">
+        <Logo className="mt-1 h-6 w-6 shrink-0" />
+        <div>
+          <h4 className="leading-0">Title title</h4>
+
+          <p className="text-subtle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, labore?</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
--- apps/nextjs/src/app/(dashboard)/dashboards/page.tsx
+++ apps/nextjs/src/app/(dashboard)/dashboards/page.tsx
@@ -1,3 +1,23 @@
+import { cn } from '@haxiom/ui';
+import { MAX_WIDTH_CLASS, MAIN_CONTENT_CLASS } from '~/app/(dashboard)/_constants';
+import { IntegrationDashboardsCard } from '~/app/(dashboard)/_components/integration-dashboards-card';
+import { PrivateDashboardsCard } from '~/app/(dashboard)/dashboards/_components/private-dashboards-card';
+import { TeamDashboardsCard } from '~/app/(dashboard)/dashboards/_components/team-dashboards-card';
+
 export default function Page() {
-  return <div>Dashboards page</div>;
+  return (
+    <main className={cn(MAX_WIDTH_CLASS, MAIN_CONTENT_CLASS)}>
+      <div className="grid gap-4 grid-cols-12">
+        <div className="col-span-full">
+          <IntegrationDashboardsCard />
+        </div>
+        <div className="col-span-full">
+          <PrivateDashboardsCard />
+        </div>
+        <div className="col-span-full">
+          <TeamDashboardsCard />
+        </div>
+      </div>
+    </main>
+  );
 }
