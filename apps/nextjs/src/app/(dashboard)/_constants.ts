da62499731500b91054e88abaeb858cd138dd120
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-27 17:26:59 +0100

    feat(app): add border decorative to constants, update MAX_WIDTH_CLASS

--- apps/nextjs/src/app/(dashboard)/_constants.ts
+++ apps/nextjs/src/app/(dashboard)/_constants.ts
@@ -1,5 +1,6 @@
-export const MAX_WIDTH_CLASS = 'max-w-6xl mx-auto px-4';
+export const MAX_WIDTH_CLASS = 'max-w-6xl mx-auto px-4 md:px-8';
 export const MAIN_CONTENT_CLASS = 'py-4';
+export const BORDER_DECORATIVE_CLASS = 'border-dashed border-gray-3';
 
 export const MOCK_DATASETS = [
   {
