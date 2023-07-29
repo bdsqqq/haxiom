d118ed7b6dedfd66cb99489c459752fead1c0e3d
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-29 13:10:01 +0100

    feat(app): controlled newDatasetsDialog so it can use Lock

--- apps/nextjs/src/app/(dashboard)/_components/datasets-table.tsx
+++ apps/nextjs/src/app/(dashboard)/_components/datasets-table.tsx
@@ -6,27 +6,39 @@ import Link from 'next/link';
 import { HardDrive, Layers } from '@haxiom/ui/icons';
 import { Card, CardContent, CardHeader, CardTitle } from '@haxiom/ui/card';
 import { NewDatasetDialog } from './new-dataset-dialog';
-import { DialogTrigger } from '@haxiom/ui/dialog';
+import { Lock } from './Lock';
+import { useState } from 'react';
+import { useDebugStore } from './debugger';
+import { mockAction } from '../_utils';
 
 const DATASET_URL_PREFIX = '/datasets';
 const MAKE_DATASET_URL = (id: string) => `${DATASET_URL_PREFIX}/${id}`;
 
 export const DatasetsTable = () => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const { hasABunchOfAccesses } = useDebugStore();
+
   return (
     <Card>
       <CardHeader className="justify-between relative">
         <CardTitle>Datasets</CardTitle>
-        <NewDatasetDialog>
-          <DialogTrigger asChild>
+        <NewDatasetDialog
+          onOpenChange={(e) => {
+            setDialogOpen(e);
+          }}
+          open={dialogOpen}
+        >
+          <Lock locked={!hasABunchOfAccesses} lockedFeedback={<NoAccessFeedback />}>
             <Button
+              onClick={() => setDialogOpen(true)}
               options={{
                 variant: 'outline',
               }}
-              className="absolute -inset-px left-auto h-auto focus:ring-offset-0"
+              className="absolute -inset-px left-auto h-auto focus:ring-offset-0 data-[state=open]:bg-element-hover"
             >
               New Dataset
             </Button>
-          </DialogTrigger>
+          </Lock>
         </NewDatasetDialog>
       </CardHeader>
       <CardContent className="p-0">
@@ -50,5 +62,25 @@ export const DatasetsTable = () => {
         ))}
       </CardContent>
     </Card>
+  );
+};
+
+const NoAccessFeedback = () => {
+  return (
+    <div className="flex flex-col gap-4 max-w-[320px]">
+      <div>
+        <h4>You're already using your two free datasets.</h4>
+        <p className="text-subtle">Unlimited datasets can be created for $25 per month.</p>
+      </div>
+
+      <Button
+        onClick={() => {
+          mockAction('Added a credit card');
+        }}
+        className="w-max"
+      >
+        Add a credit card
+      </Button>
+    </div>
   );
 };
