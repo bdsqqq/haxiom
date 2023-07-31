f25068510afeb23ab905c879c9a68a0aef88b54e
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-31 12:02:58 +0100

    feat(app): top bars border subtle

--- apps/nextjs/src/app/(dashboard)/_components/top-tool-bar.tsx
+++ apps/nextjs/src/app/(dashboard)/_components/top-tool-bar.tsx
@@ -4,7 +4,7 @@ import { MAX_WIDTH_CLASS } from '../_constants';
 
 export const TopToolBar = ({ children, className }: { children?: ReactNode; className?: string }) => {
   return (
-    <div className="h-16 border-b sticky top-12 z-20">
+    <div className="h-16 border-b border-subtle sticky top-12 z-20">
       <div className={cn('flex h-full items-center bg', MAX_WIDTH_CLASS, className)}>{children}</div>
     </div>
   );
