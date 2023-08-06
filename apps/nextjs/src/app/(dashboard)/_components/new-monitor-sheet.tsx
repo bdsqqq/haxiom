c139efa669b22e111e9a94b2f61a084a648f3aa6
Igor Bedesqui <igorbedesqui@gmail.com>
2023-08-06 19:27:49 +0100

    feat(app): monitor form actually uses hook-form

--- apps/nextjs/src/app/(dashboard)/_components/new-monitor-sheet.tsx
+++ apps/nextjs/src/app/(dashboard)/_components/new-monitor-sheet.tsx
@@ -2,56 +2,102 @@
 
 import { Sheet, SheetClose, SheetContent, SheetTitle } from '@haxiom/ui/sheet';
 import type { ComponentProps, ReactNode } from 'react';
-import { Label } from '@haxiom/ui/label';
 import { Input, TextArea } from '@haxiom/ui/input';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@haxiom/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
 import { Button } from '@haxiom/ui/button';
 import { mockAction } from '../_utils';
+import { useForm } from 'react-hook-form';
+import { FormDevTool } from '~/components/hookform-devtool';
+
+const MAX_DESCRIPTION_LENGTH = 400;
+
+const formSchema = z.object({
+  name: z.string().nonempty('Please provide a name for your monitor.'),
+  description: z.string().max(MAX_DESCRIPTION_LENGTH, 'Please keep your description under 400 characters.'),
+});
 
 /**
  * Use this a a controlled component or provide a SheetTrigger as a child to this component.
  */
 export const NewMonitorSheet = ({ children, ...rest }: { children?: ReactNode } & ComponentProps<typeof Sheet>) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    mockAction('Created a monitor called ' + values.name);
+  }
+
   return (
     <Sheet {...rest}>
       {children}
       <SheetContent>
         <SheetTitle>New Monitor</SheetTitle>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            mockAction('Created a monitor');
-          }}
-          className="flex flex-col gap-4"
-        >
-          <fieldset>
-            <Label htmlFor="name">Name</Label>
-            <Input className="ring-offset-0" id="name" placeholder="Triangle sells..." />
-          </fieldset>
-
-          <fieldset>
-            <Label htmlFor="name">Description</Label>
-            <TextArea
-              className="ring-offset-0 resize-none"
-              id="name"
-              placeholder="keep track of how many people buy triangles from the Triangle company..."
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input className="ring-offset-0" placeholder="Triangle sells..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </fieldset>
 
-          <div className="flex justify-between flex-row-reverse w-full">
-            <Button type="submit">Create</Button>
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="description">Description</FormLabel>
+                  <FormControl>
+                    <TextArea
+                      className="ring-offset-0 resize-none"
+                      {...field}
+                      maxLength={MAX_DESCRIPTION_LENGTH}
+                      placeholder="keep track of how many people buy triangles from the Triangle company..."
+                    />
+                  </FormControl>
+                  <FormDescription className="flex justify-between">
+                    <span>Max {MAX_DESCRIPTION_LENGTH} characters.</span>{' '}
+                    <span className="tabular-nums">
+                      {field.value.length}/{MAX_DESCRIPTION_LENGTH}
+                    </span>
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-            <SheetClose asChild>
-              <Button
-                options={{
-                  variant: 'outline',
-                }}
-                type="button"
-              >
-                Cancel
-              </Button>
-            </SheetClose>
-          </div>
-        </form>
+            <div className="flex justify-between flex-row-reverse w-full">
+              <Button type="submit">Create</Button>
+
+              <SheetClose asChild>
+                <Button
+                  options={{
+                    variant: 'outline',
+                  }}
+                  type="button"
+                >
+                  Cancel
+                </Button>
+              </SheetClose>
+            </div>
+          </form>
+        </Form>
+
+        <FormDevTool control={form.control} />
       </SheetContent>
     </Sheet>
   );
