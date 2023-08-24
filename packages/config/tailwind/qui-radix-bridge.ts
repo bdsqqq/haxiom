c7f42c43ea3b963c7d55bdacc8fdb692210fb92d
Igor Bedesqui <igorbedesqui@gmail.com>
2023-08-24 20:04:21 +0100

    feat(tw): more scoped semantic values

--- packages/config/tailwind/qui-radix-bridge.ts
+++ packages/config/tailwind/qui-radix-bridge.ts
@@ -109,6 +109,14 @@ const semanticSteps: SemanticSteps = {
     { key: "background-element-selected", step: "5" },
     { key: "separator-subtle", step: "6" },
   ],
+  ringOffsetColor: [
+    { key: "background-base", step: "1" },
+    { key: "background-subtle", step: "2" },
+    { key: "background-element", step: "3" },
+    { key: "background-element-hover", step: "4" },
+    { key: "background-element-active", step: "5" },
+    { key: "background-element-selected", step: "5" },
+  ],
   borderColor: [
     { key: "DEFAULT", step: "6" },
     { key: "separator-subtle", step: "6" },
@@ -116,13 +124,35 @@ const semanticSteps: SemanticSteps = {
     { key: "border-element", step: "7" },
     { key: "border-element-hover", step: "8" },
   ],
+  ringColor: [
+    { key: "DEFAULT", step: "7" },
+    { key: "base", step: "7" },
+  ],
   colors: [
     { key: "solid", step: "9" },
     { key: "solid-hover", step: "10" },
   ],
+  boxShadowColor: [
+    { key: "DEFAULT", step: "9" },
+    { key: "boxShadow-subtle", step: "9" },
+    { key: "boxShadow-contrast", step: "10" },
+  ],
+  accentColor: [
+    { key: "DEFAULT", step: "9" },
+    { key: "accent-base", step: "9" },
+    { key: "accent-hover", step: "10" },
+  ],
   textColor: [
     { key: "text-subtle", step: "11" },
     { key: "text-contrast", step: "12" },
+  ],
+  textDecorationColor: [
+    { key: "textDecoration-subtle", step: "11" },
+    { key: "textDecoration-contrast", step: "12" },
+  ],
+  placeholderColor: [
+    { key: "placeholder-subtle", step: "11" },
+    { key: "placeholder-contrast", step: "12" },
   ],
 };
 
